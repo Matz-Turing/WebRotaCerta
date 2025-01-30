8d081ce7d8377e9c3348e750efdf06a0e98bf475
𝑀𝑎𝑡𝑒𝑢𝑠 𝑆. <mateuscontato@keemail.me>
2025-01-29 23:36:14 -0300

    Add files via upload

---
+++ RotaCerta/script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.app-button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Redirecionando para o download do aplicativo...');
+    });
+});
